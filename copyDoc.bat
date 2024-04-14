1d908d874b4febd54f08f9276f82ac3f63adc7e9
CJL <2662442385@qq.com>
2024-04-14 16:16:22 +0800

    update bat and desc

--- copyDoc.bat
+++ copyDoc.bat
@@ -1,4 +1,5 @@
 REM /E 参数用于复制所有子目录（即使是空的）
 REM /I 参数如果目标不存在，会创建目录
 REM /Y 参数用于覆盖已存在的文件而不需要提示
+xcopy /E /I /Y C:\Code\FrontEnd\jl-tool\docs\.vitepress\config.mts C:\Code\FrontEnd\beixiyo.github.io\docs\.vitepress\config.mts
 xcopy /E /I /Y C:\Code\FrontEnd\jl-tool\docs\type-doc C:\Code\FrontEnd\beixiyo.github.io\docs\type-doc
